d73a9d15600cf2376e9c8a278bb4e50718c64385
Evan Sheffield <evan.c.sheffield@gmail.com>
2018-06-06 00:22:54 -0400

    partial: Generate episode from policy for validation

--- src/pcgapprentice/DungeonIRL.java
+++ src/pcgapprentice/DungeonIRL.java
@@ -5,16 +5,23 @@ import java.util.ArrayList;
 
 import burlap.behavior.functionapproximation.dense.DenseStateFeatures;
 import burlap.behavior.policy.Policy;
+import burlap.behavior.policy.PolicyUtils;
 import burlap.behavior.singleagent.Episode;
 import burlap.behavior.singleagent.learnfromdemo.apprenticeship.ApprenticeshipLearning;
 import burlap.behavior.singleagent.learnfromdemo.apprenticeship.ApprenticeshipLearningRequest;
 import burlap.behavior.singleagent.learning.tdmethods.QLearning;
 import burlap.mdp.auxiliary.StateGenerator;
+import burlap.mdp.core.TerminalFunction;
 import burlap.mdp.singleagent.SADomain;
+import burlap.mdp.singleagent.common.UniformCostRF;
+import burlap.mdp.singleagent.model.FactoredModel;
+import burlap.mdp.singleagent.model.RewardFunction;
 import burlap.statehashing.simple.SimpleHashableStateFactory;
 import pcgapprentice.dungeonlevel.DungeonDomainGenerator;
 import pcgapprentice.dungeonlevel.DungeonFeatures;
 import pcgapprentice.dungeonlevel.DungeonStartStateGenerator;
+import pcgapprentice.dungeonlevel.DungeonStateModel;
+import pcgapprentice.dungeonlevel.DungeonTF;
 import pcgapprentice.dungeonlevel.utils.EpisodeReader;
 
 public class DungeonIRL {
@@ -34,8 +41,15 @@ public class DungeonIRL {
 			ApprenticeshipLearningRequest request = new ApprenticeshipLearningRequest(domain, planner, features, expertEpisodes, startStateGenerator);
 
 			Policy learnedPolicy = ApprenticeshipLearning.getLearnedPolicy(request);
-			System.out.println("Done!");
 
+			DungeonStateModel stateModel = new DungeonStateModel();
+			// The reward function doesn't really matter here - the goal is to learn a real one!
+			RewardFunction rf = new UniformCostRF();
+			TerminalFunction tf = new DungeonTF();
+
+			Episode giveitago = PolicyUtils.rollout(learnedPolicy, startStateGenerator.generateState(), new FactoredModel(stateModel, rf, tf));
+
+			System.out.println("Done!");
 		} catch (IOException e) {
 			System.err.println("Unable to read episode file: " + e.getMessage());
 			e.printStackTrace();

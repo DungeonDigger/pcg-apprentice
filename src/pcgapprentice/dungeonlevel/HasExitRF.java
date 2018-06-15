2d3a622fa5e64f3abc537564701154365ad1f8db
Evan Sheffield <evan.c.sheffield@gmail.com>
2018-06-15 18:24:32 -0400

    refactor: Make custom reward value for exit parameterized

--- src/pcgapprentice/dungeonlevel/HasExitRF.java
+++ src/pcgapprentice/dungeonlevel/HasExitRF.java
@@ -4,13 +4,23 @@ import burlap.mdp.core.action.Action;
 import burlap.mdp.core.state.State;
 import burlap.mdp.singleagent.model.RewardFunction;
 
+/**
+ * A custom reward function for DungeonLimitedStates to apply
+ * a reward when the state contains an exit tile and zero otherwise.
+ */
 public class HasExitRF implements RewardFunction {
+
+	private double exitReward;
+
+	public HasExitRF(double exitReward) {
+		this.exitReward = exitReward;
+	}
 
 	@Override
 	public double reward(State s, Action a, State sprime) {
 		DungeonLimitedState ds = (DungeonLimitedState)sprime;
 		if(ds.hasExit) {
-			return 10;
+			return exitReward;
 		}
 		return 0;
 	}
